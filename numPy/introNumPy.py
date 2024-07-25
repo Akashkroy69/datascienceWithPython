ab0a16b4390d59642f31bea33397c1decfd5c223
akashkroy69 <akashkroy@hotmail.com>
2024-07-25 18:18:31 +0530

    initial commit in data science

---
+++ numPy/introNumPy.py
@@ -0,0 +1,13 @@
+import numpy as np
+# 1 D array
+array = np.array([1,2,3,4,5,6])
+print(array)
+
+# 2 D array
+
+array_2D = np.array([[1,2,3],[4,5,6]])
+print(array_2D)
+
+# zeros
+zeros = np.zeros((2,3))
+print(zeros)
